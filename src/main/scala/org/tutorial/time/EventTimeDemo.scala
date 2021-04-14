029670aa2c2fcc1f58b46d97dc76c656e30f4500
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-04-14 09:47:47 +0800

    window

--- src/main/scala/org/tutorial/time/EventTimeDemo.scala
+++ src/main/scala/org/tutorial/time/EventTimeDemo.scala
@@ -1,6 +1,9 @@
 package org.tutorial.time
 
+import java.time.Duration
+
 import org.apache.flink.api.common.RuntimeExecutionMode
+import org.apache.flink.api.common.eventtime.{SerializableTimestampAssigner, WatermarkStrategy}
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
@@ -20,8 +23,20 @@ object EventTimeDemo {
       }
     )
 
-    ds.keyBy(_.userId).window(TumblingProcessingTimeWindows.of(Time.seconds(100)))
-      .reduce((a, b) => a.add(b)).print()
+    // 在流开始的时候指定水印
+    val dsEvent: DataStream[MyEvent] = ds.assignTimestampsAndWatermarks(WatermarkStrategy
+      .forBoundedOutOfOrderness[MyEvent](Duration.ofSeconds(20))
+      .withTimestampAssigner(new SerializableTimestampAssigner[MyEvent] {
+        override def extractTimestamp(element: MyEvent, recordTimestamp: Long): Long = element.createTime
+      })
+    )
+
+    dsEvent.keyBy(_.userId).window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+    //    ds.keyBy(_.userId).window(TumblingProcessingTimeWindows.of(Time.seconds(100)))
+    //      .reduce((a, b) => a.add(b))
+    //        .assignTimestampsAndWatermarks()
+
+    // 注意：时间戳和 watermark 都是从 1970-01-01T00:00:00Z 起的 Java 纪元开始，并以毫秒为单位。
 
 
     env.execute()
