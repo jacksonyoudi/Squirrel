947747c1f52747474c984bace4ead417fb914fcd
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-05-31 21:45:32 +0800

    sql

--- src/main/scala/youdi/may/ch07/D02.scala
+++ src/main/scala/youdi/may/ch07/D02.scala
@@ -6,9 +6,9 @@ import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
 object D02 {
   def main(args: Array[String]): Unit = {
 
-    
+
     val settings: EnvironmentSettings = EnvironmentSettings.newInstance.inStreamingMode.build
     StreamTableEnvironment.create(settings)
-
+  
   }
 }
